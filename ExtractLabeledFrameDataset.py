326889ef155f5f74fcc153b664669ea4078f5307
Knowblesse <knowblesse@gmail.com>
2021-08-25 13:30:39 +0900

    Frame Extraction --> removed multiprocessing
    I don't know why, but the multiprocessing function is not working!

--- ExtractLabeledFrameDataset.py
+++ ExtractLabeledFrameDataset.py
@@ -11,37 +11,29 @@ import numpy as np
 import datetime
 
 # project configuration
-project_name = 'Dev_LARGE'
-path_video = Path('/mnt/Data/Data/Small Experiment/LARGE/2021-04-22 11-47-11.mkv')
-path_label = Path('/mnt/Data/Data/Small Experiment/LARGE/2021-04-22 11-47-11.mkv_data.csv')
-
-path_export = Path.joinpath(Path.home(),'VCF/butter/dataset')
+project_name = 'ImplantedRat'
+path_video = Path('/mnt/Data/Data/Lobster/Lobster_Recording-200319-161008/21JAN5/#21JAN5-210629-183643/Lobster_Recording-210330-101307_21JAN5-210629-183643_Vid1.avi')
+path_label = Path('/mnt/Data/Data/Lobster/Lobster_Recording-200319-161008/21JAN5/#21JAN5-210629-183643/Lobster_Recording-210330-101307_21JAN5-210629-183643_Vid1.avi_data.csv')
+path_export = Path.joinpath(Path.home(),'VCF/butter/dataset/ImplantedRat')
 
 starttime = datetime.datetime.now()
-
-def acquireVideoCapture():
-    global vid
-    vid = cv.VideoCapture(str(path_video))
-
-def saveFrame(datanum, framenum):
-    #vid = cv.VideoCapture(str(path_video))
-    vid.set(cv.CAP_PROP_POS_FRAMES,framenum)
-    _, frame = vid.read()
-    result = cv.imwrite(str(Path.joinpath(path_export,'{:s}_{:04d}.png'.format(project_name, datanum))),frame)
-    if not result:
-        raise(Exception('Can not save the frame'))
 
 # Load the Dataset
 csv_data = np.loadtxt(str(path_label),delimiter=',')
 labeledData = np.where(csv_data[:,3] == 1)[0]
 
 print('ExtractLabeledFrameDataset.py : Detected {:d} labeled image\n'.format(len(labeledData)))
-print('ExtractLabeledFrameDataset.py : Extract labeld frames to the {:s}\n'.format(str(path_export)))
+print('ExtractLabeledFrameDataset.py : Extract labeled frames to the {:s}\n'.format(str(path_export)))
 
-# Extract labeled frames using Multiprocessing
-pool = multiprocessing.Pool(processes=multiprocessing.cpu_count(),initializer=acquireVideoCapture)
-pool.starmap(saveFrame,zip(np.arange(len(labeledData)), labeledData))
-pool.close()
+vid = cv.VideoCapture(str(path_video))
+zip(np.arange(len(labeledData)), labeledData)
+
+for datanum, framenum in enumerate(labeledData):
+    vid.set(cv.CAP_PROP_POS_FRAMES,framenum)
+    _, frame = vid.read()
+    result = cv.imwrite(str(Path.joinpath(path_export,'{:s}_{:04d}.png'.format(project_name, datanum))),frame)
+    if not result:
+        raise(Exception('Can not save the frame'))
 
 # Save location file
 np.savetxt(str(Path.joinpath(path_export,'{:s}_label.csv'.format(str(path_export)))),csv_data[labeledData,:-1], delimiter='\t')
