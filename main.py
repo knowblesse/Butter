b12e683a3ad35e91e103c3b27af7059e09882dc8
Knowblesse <knowblesse@gmail.com>
2023-07-10 14:34:32 +0900

    new main.py with new functions

--- main.py
+++ main.py
@@ -1,35 +1,46 @@
 import datetime
 import time
+import pickle
 from pathlib import Path
-from Butter import VideoProcessor
+from Butter import Butter
 from tkinter.filedialog import askdirectory
 
 # Path for the folder which has the video
 video_path = Path(askdirectory())
 
-# Path for the model folder 
+# Path for the model folder
 model_path = Path('./Models/Model_230201_1649')
 
 # Create VideoProcessor Instance
-vp = VideoProcessor(video_path, model_path)
+butter = Butter(video_path, model_path)
 
-# Build a foreground Model of the animal
-#vp.buildForegroundModel()
+# Set global mask
+butter.setGlobalMask()
 
-animalThreshold = 31
-p2pDisplacement={'median': 17.20, 'sd': 312.87}
-animalSize={'median': 2750, 'sd': 312.87}
-animalConvexity={'median': 0.88, 'sd': 0.09}
-animalCircularity={'median': 0.49, 'sd': 0.09}
-vp.setForegroundModel(animalThreshold, p2pDisplacement, animalSize, animalConvexity, animalCircularity)
+## If Background and foreground model exist, use it.
+if Path('fg.pk').exists() and Path('bg.pk').exists():
+    with open('fg.pk', 'rb') as f:
+        butter.setForegroundModel(pickle.load(f))
+    with open('bg.pk', 'rb') as f:
+        butter.setBackgroundModel(pickle.load(f))
+else: # Make background and foreground model
+    butter.saveSampleFrames()
 
+    butter.buildBackgroundModel()
 
-# Check the starting frame. (if the animal is present from the beginning of the video, just type zero
-vp.checkStartPosition()
+    butter.buildForegroundModel()
+
+    with open('fg.pk', 'wb') as f:
+        pickle.dump(butter.getForegroundModel(), f)
+    with open('bg.pk', 'wb') as f:
+        pickle.dump(butter.getBackgroundModel(draw=False), f)
+
+# Start Position
+butter.checkStartPosition()
 
 # Start the processing
 starttime = time.time()
-vp.run()
+butter.run()
 print(str(datetime.timedelta(seconds=time.time() - starttime)))
-vp.save()
+butter.save()
 print('Saved!\n\n')
